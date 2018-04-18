9e56a5bad2a6db340b927a74b8ffc05b6fa93e12
Your Name <you@example.com>
2018-04-18 22:25:42 +0800

    1

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -15,15 +15,14 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*wx$ll4b76^hfaf3)la7!jw2lyoou=e*)vh_v-4nhgcp5&na67'
-
+# SECRET_KEY = '*wx$ll4b76^hfaf3)la7!jw2lyoou=e*)vh_v-4nhgcp5&na67'
+SECRET_KEY = 'i3k%m-808v3_)^h7975iw4v&fl5chq41^19j@u+b*vx7dvw*q$'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.zhongminyong.tech']
 
@@ -112,12 +111,11 @@ TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+#zmy
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
